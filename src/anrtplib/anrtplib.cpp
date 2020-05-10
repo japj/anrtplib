4349fa0f4846e4d8eefa6151ddd639f0220337e1
Jeroen Janssen <jeroen.janssen@gmail.com>
2020-05-10 09:58:16 +0200

    setup initial project and test

---
+++ src/anrtplib/anrtplib.cpp
@@ -0,0 +1,12 @@
+﻿// anrtplib.cpp : Defines the entry point for the application.
+//
+
+#include "anrtplib.h"
+
+using namespace std;
+
+int domain()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ test/test.cpp
@@ -0,0 +1,10 @@
+﻿// anrtplib.cpp : Defines the entry point for the application.
+//
+#include <gtest/gtest.h>
+
+#include <anrtplib.hpp>
+
+TEST(MAIN, SomeTest) {
+	ASSERT_EQ(1,2);
+	ASSERT_NE(1,1);
+}
